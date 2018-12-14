2f5f4f687f3ea230c054479cc8b10df04f3ee39a
Jonathan Robertson <42648270+JonRobertson93@users.noreply.github.com>
2018-12-14 11:29:17 -0600

    Add new rows JS
    Implement JS so add new rows button creates new HTML in the form

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Model: If option 'Bose' selected create all Bose models as option values
 // DOM elements
-const brands = document.getElementById("brands");
-const models = document.getElementById("models");
-const qty = document.getElementById('qty');
+let submissionForm = document.getElementById("submission-form");
+let brands = document.getElementsByClassName("brands");
+let models = document.getElementsByClassName("models");
+let qty = document.getElementsByClassName('qty');
 const quoteButton = document.getElementById('quoteButton');
 let quoted = document.getElementById('quote');
-let addMoreButton = document.getElementById('addMore');
+const addMoreButton = document.getElementById('addMore');
+let rowCount = 0;
 
 
 // Headphones RAW data
@@ -36,7 +38,6 @@ class CartItem {
 let allCartItems = [];
 
 
-
 // More headphones data - tuples basically
 const QC25 = new Headphones("QC25", 20);
 const QC35 = new Headphones("QC35", 40);
@@ -51,63 +52,69 @@ const allHeadphones = [Solo2, Solo3, Studio2, Studio3, QC25, QC35, MDRX];
 let selectedBrand;
 let selectModel;
 
-brands.addEventListener('change', addModels);
+Array.from(brands).forEach(function(brand) {
+      brand.addEventListener('change', addModels);
+    });
+
 addMoreButton.addEventListener('click', addMore);
 
-quoteButton.addEventListener('click', generateQuote);
-	selectModel = models.options[models.selectedIndex].innerHTML;
-	for (let i = 0; i < allHeadphones.length; i++) {
-		if (selectModel == allHeadphones[i].model) {
-			firstTotalPrice = allHeadphones[i].price;
-			console.log(firstTotalPrice);
-		}
-	}
-	firstTotalPrice *= qty.value;
-	clearQuotes();
-	setTimeout(generateQuote, 1000);
-
-
-
-
 //TODO - Move functions down here
-function addModels() {
+function addModels(e) {
 	deleteModels();
-	selectedBrand = brands.options[brands.selectedIndex].value;
+	selectedBrand = e.target.options[e.target.selectedIndex].value;
 	let current = brandModels[selectedBrand];
-	models.options.add(new Option());	// Adds one blank option at top
+	//add correct brand's models to current models select box:
+	models[rowCount].options.add(new Option());	// Adds one blank option at top
 	for (i = 0; i < current.length; i ++) {
-		models.options.add(new Option(current[i]));
+		models[rowCount].options.add(new Option(current[i]));
 	}
+	rowCount++;
+};
+
+function deleteModels(e) {
+	Array.from(models).forEach(function(model) {
+		models.innerHTML = "";
+	});
 }
 
-function deleteModels() {
-	models.innerHTML = "";
-}
-
-function generateQuote() {
-	quoted.innerHTML = `We can offer $${firstTotalPrice} for your ${qty.value} ${selectedBrand} ${selectModel} headphones.`;
-	// TESTING - WORKING SO FAR!
-	for (let i = 0; i <allCartItems.length; i++) {
-		console.log(allCartItems[i].model + " x " + allCartItems[i].quantity);	
-	}
-}
-
-function clearQuotes() {
-	quoted.innerHTML = "";
-}
 
 function addMore() {
-	selectModel = models.options[models.selectedIndex].innerHTML;
-	for (let i = 0; i < allHeadphones.length; i++) {
-		if (selectModel == allHeadphones[i].model) {
-			firstTotalPrice = allHeadphones[i].price;
-			console.log(firstTotalPrice);
-		}
-	}
-	let item = new CartItem(selectModel, qty.value);
-	allCartItems.push(item);
+	addMoreButton.insertAdjacentHTML('beforebegin', `<label class="label" for="brands"> Brand: </label>
+			<select class="brands" name="brand[]">
+				<option></option>
+				<option value="Bose">Bose</option>
+				<option value="Beats by Dre">Beats by Dre</option>
+				<option value="Sony">Sony</option>
+				<option value="JBL">JBL</option>
+			</select>
+
+			<label class="label" for="models"> Model: </label>
+			<select class="models" name="model[]">
+			</select>
+
+			<label class="label" for="qty"> Quantity: </label>
+			<input name="qty" type="text" class="qty" name="qty[]">
+			<br />`);
+	brands = document.getElementsByClassName("brands");
+	models = document.getElementsByClassName("models");
+	qty = document.getElementsByClassName('qty');
+
+	Array.from(brands).forEach(function(brand) {
+      brand.addEventListener('change', addModels);
+    });
 
 }
+
+// OLD ADDMORE() FUNCTION:
+	// selectModel = models.options[models.selectedIndex].innerHTML;
+	// for (let i = 0; i < allHeadphones.length; i++) {
+	// 	if (selectModel == allHeadphones[i].model) {
+	// 		firstTotalPrice = allHeadphones[i].price;
+	// 		console.log(firstTotalPrice);
+	// 	}
+	// }
+	// let item = new CartItem(selectModel, qty.value);
+	// allCartItems.push(item);
 
 
 
@@ -116,3 +123,32 @@ function addMore() {
 // Add this value to a confirmation page
 // Do this for all items added in get quote.
 // Reroute to confirmation page.
+
+
+// QUOTE INFO - May use some of this later 
+
+// quoteButton.addEventListener('click', generateQuote);
+// 	selectModel = models.options[models.selectedIndex].innerHTML;
+// 	for (let i = 0; i < allHeadphones.length; i++) {
+// 		if (selectModel == allHeadphones[i].model) {
+// 			firstTotalPrice = allHeadphones[i].price;
+// 			console.log(firstTotalPrice);
+// 		}
+// 	}
+// 	firstTotalPrice *= qty.value;
+// 	clearQuotes();
+// 	setTimeout(generateQuote, 1000);
+
+// function generateQuote() {
+// 	quoted.innerHTML = `We can offer $${firstTotalPrice} for your ${qty.value} ${selectedBrand} ${selectModel} headphones.`;
+// 	// TESTING - WORKING SO FAR!
+// 	for (let i = 0; i <allCartItems.length; i++) {
+// 		console.log(allCartItems[i].model + " x " + allCartItems[i].quantity);	
+// 	}
+// }
+
+// function clearQuotes() {
+// 	quoted.innerHTML = "";
+// }
+
+
