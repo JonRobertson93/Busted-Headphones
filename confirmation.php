b243f354335655aeec3c8f95d0e38c4737c05d62
Jonathan Robertson <42648270+JonRobertson93@users.noreply.github.com>
2019-07-01 10:48:40 -0500

    file update for personal site

--- confirmation.php
+++ confirmation.php
@@ -1,9 +1,8 @@
 <?php
-// Server and database info
-$servername = "";
-$database = "";
-$username = "";
-$password = "";
+$servername = "localhost";
+$database = "u785025458_cart";
+$username = "u785025458_jon";
+$password = "EXgg8s65UnDY";
 
 // Create connection
 
@@ -55,23 +54,32 @@ if (!mysqli_query($conn, $update_order_id)) {
 	<title> Sell Junk Headphones </title>
 	<link href="style.css" rel="stylesheet">
 	<link href="responsive.css" rel="stylesheet">
+	<link href="../navbar.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 <body>
-	<div id="navbar">
-		<div id="nav-link-1">
-			<span> <i class="fas fa-headphones-alt"></i> <a href="http://brokenheadphones.store/"> Busted Headphones</a> </span>
-		</div>
-		<div class="nav-link"> <a href="http://brokenheadphones.store/"> Home </a> </div>
-		<div class="nav-link"> <a href="#"> FAQs </a> </div>
-		<div class="nav-link"> <a href="http://brokenheadphones.store/contact.html"> Contact </a> </div>
-		<a href="javascript:void(0);" id="icon"> <i class="fas fa-bars"></i></a>
-	</div>
+	<nav id="navbar">
+        <h1> Animal Clicker </h1>
+        <a href="../" id="home"> Home </a>
+        <p id="projects"> Projects <i id="downArrow" class="fa fa-caret-down"></i></p>
+        <ul class="dropdown">
+            <li> Animal Clicker </li>
+            <li> Art Portfolio </li>
+            <li> Broken Headphones </li>
+            <li> Concentration Game </li>
+            <li> eBay Calculator </li>
+            <li> Frogger Game </li>
+            <li> HTML Email Blast Ad </li>
+            <li> Responsive News Site </li>
+            <li> Restroom Rater </li>
+            <li> Sample Portfolio Page </li>
+        </ul>
+    </nav>
 	<!-- Main body content -->
     <div id="container">
 	<div id="confirmation">
-		<h1> Order Confirmation </h1>
+		<h2> Order Confirmation </h2>
 		<div id="personalDetails">
 			<p> <b> Thank you for your order! </b> <br/> Your order has successfully been placed and you will receive an email within 24 hours with a pre-paid shipping label to mail in your headphones. </p>
 			<hr>
